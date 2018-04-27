e24a2914c5cbc77e0c387d5dfb281518b27bd168
chengmboy <cmn@coinfex.com>
2018-04-27 18:07:07 +0800

    自定义协议实现TCP长连接双向通话

--- javacore/src/main/java/com/chengmboy/network/SocketClient.java
+++ javacore/src/main/java/com/chengmboy/network/SocketClient.java
@@ -1,19 +1,71 @@
 package com.chengmboy.network;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author cheng_mboy
  */
 public class SocketClient {
 
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 8080);
-        OutputStream outputStream = socket.getOutputStream();
-        outputStream.write(97);
-        outputStream.flush();
-        socket.shutdownOutput();
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(100, 100,
+                0, TimeUnit.MICROSECONDS, new LinkedBlockingQueue<>(), new ChatClientThreadFactory());
+
+        Socket socket = new Socket("localhost", 3000);
+
+        poolExecutor.submit(() -> {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                while (scanner.hasNextLine()) {
+                    String say = scanner.nextLine();
+                    OutputStream outputStream = socket.getOutputStream();
+                    outputStream.write(say.getBytes());
+                    outputStream.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        poolExecutor.submit(() -> {
+            try {
+                InputStream inputStream = socket.getInputStream();
+                int read;
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                while ((read = inputStream.read()) != -1) {
+                    if (read == 13) {
+                        System.out.println(socket.getRemoteSocketAddress() + "说: " + new String(byteArrayOutputStream.toByteArray()));
+                        byteArrayOutputStream = new ByteArrayOutputStream();
+                    } else {
+                        byteArrayOutputStream.write(read);
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+    }
+
+    private static class ChatClientThreadFactory implements ThreadFactory {
+
+        private final AtomicInteger threadNumber = new AtomicInteger(0);
+
+        @Override
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "chat_client_pool_" + threadNumber.getAndIncrement());
+        }
     }
 }
--- javacore/src/main/java/com/chengmboy/network/SocketServer.java
+++ javacore/src/main/java/com/chengmboy/network/SocketServer.java
@@ -1,9 +1,15 @@
 package com.chengmboy.network;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 /**
@@ -11,15 +17,55 @@ import java.net.Socket;
  */
 public class SocketServer {
 
+
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8888);
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(100, 100,
+                0, TimeUnit.MICROSECONDS, new LinkedBlockingQueue<>(), new ChatServerThreadFactory());
+        ServerSocket serverSocket = new ServerSocket();
+        serverSocket.bind(new InetSocketAddress(3000));
+
         while (true) {
             Socket accept = serverSocket.accept();
-            InputStream inputStream = accept.getInputStream();
-            int read;
-            while ((read = inputStream.read()) != -1) {
-                System.out.print(read);
+            try {
+                InputStream inputStream = accept.getInputStream();
+                OutputStream outputStream = accept.getOutputStream();
+
+                poolExecutor.submit(() -> {
+                    Scanner scanner = new Scanner(inputStream);
+                    while (scanner.hasNextLine()) {
+                        System.out.println(scanner.nextLine());
+                    }
+                });
+
+                poolExecutor.submit(() -> {
+                    try {
+                        Scanner scanner = new Scanner(System.in);
+                        while (scanner.hasNext()) {
+                            int i = scanner.nextInt();
+                            outputStream.write(i);
+                            outputStream.flush();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
+
+    private static class ChatServerThreadFactory implements ThreadFactory {
+
+        private final AtomicInteger threadNumber = new AtomicInteger(0);
+
+        @Override
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "chat_server_pool_" + threadNumber.getAndIncrement());
+        }
+    }
+
 }
+
